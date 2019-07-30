9371116d41a9a781cf1b08f7bf6644bef92d0a20
Khai Nguyen <khaimap2015@gmail.com>
2019-07-30 10:21:09 +0700

    commit_4

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,6 +16,10 @@
 
 package com.example;
 
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -74,6 +78,14 @@ public class Main {
     }
   }
 
+  @RequestMapping("/hello")
+  String hello(Map<String, Object> model) {
+      RelativisticModel.select();
+      Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+      model.put("science", "E=mc^2: 12 GeV = " + m.toString());
+      return "hello";
+  }
+
   @Bean
   public DataSource dataSource() throws SQLException {
     if (dbUrl == null || dbUrl.isEmpty()) {
@@ -85,5 +97,7 @@ public class Main {
       return new HikariDataSource(config);
     }
   }
+
+
 
 }
